b7466448f59f4f117a4feff32f3bc3560eb1ec12
James Hurst <james@pintsized.co.uk>
2011-07-02 20:30:30 +0100

    Simple mq tests

--- tests/zmq_publish.lua
+++ tests/zmq_publish.lua
@@ -3,8 +3,8 @@ require("zmq")
 local ctx = zmq.init(1)
 local pub = ctx:socket(zmq.PUB)
 
-pub:connect("tcp://127.0.0.1:5555")
+pub:bind("tcp://127.0.0.1:5555")
 while true do
 	local msg = io.read()
-	pub:send("mybighash "..msg)
+	pub:send(msg)
 end
--- tests/zmq_subscribe.lua
+++ tests/zmq_subscribe.lua
@@ -3,23 +3,10 @@ require("zmq")
 local ctx = zmq.init(1)
 
 local sub = ctx:socket(zmq.SUB)
-sub:setopt(zmq.SUBSCRIBE, "mybighash")
+sub:setopt(zmq.SUBSCRIBE, "")
 sub:bind("tcp://127.0.0.1:5555")
 
-term = false
 while true do
-	if term then
-		sub:close()
-		ctx:term()
-		break
-	end
-	
     local msg = sub:recv()
 	print(msg)
-	
-	for m in string.gmatch(msg, "%a+") do
-		if m == 'SAVED' then
-			term = true
-		end
-	end
 end
