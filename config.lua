20a772dca6c30979f5fb3faced2af8a8e4986548
James Hurst <james@pintsized.co.uk>
2011-07-03 12:40:43 +0100

    Refactored config

--- config.lua
+++ config.lua
@@ -2,13 +2,17 @@ module("config", package.seeall)
 
 -- Configuration table
 local conf = {}
+
 conf.prefix = "/__ledge" -- Nginx internal location prefix
-conf.proxy = {
-    loc = conf.prefix .. "/proxy" -- proxy location
-} 
-conf.redis = {
-    loc = conf.prefix .. "/redis",
-    max_stale_age = 900, -- How stale a valid cache entry can be
+
+conf.locations = {
+	origin = conf.prefix .. "/proxy",
+	wait_for_origin = conf.prefix .. "/wait_for_origin",
+	redis = conf.prefix .. "/redis"
 }
+
+conf.max_stale_age = 900
+
+conf.collapse_forwarding = true
 
 return conf
