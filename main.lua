abf5e937540de7746a6ea994d029d6a626b023f9
James Hurst <jhurst@sl6.vm>
2011-11-23 13:59:12 +0000

    Some fixes towards transparency. This all needs rethinking though.

--- main.lua
+++ main.lua
@@ -4,7 +4,17 @@ local ledge = require("ledge")
 ledge.process_config()
 
 -- keys is a table of cache keys indentifying this request in redis
-local keys = ledge.create_keys(ngx.var.scheme..'://'..ngx.var.host..ngx.var.uri)
+local full_uri = ngx.var.scheme..'://'..ngx.var.host
+if (ngx.var.request_uri ~= '/') then 
+    -- We don't want to accidentally add a trailing slash, so only add the path
+    -- if there's something there.
+    full_uri = full_uri .. ngx.var.request_uri
+end
+--ngx.log(ngx.NOTICE, ngx.var.args)
+--if (ngx.var.args ~= "") then
+--    full_uri = full_uri..'?'..ngx.var.args
+--end
+local keys = ledge.create_keys(full_uri)
 
 -- Prepare fetches from cache, so we're either primed with a full response
 -- to send, or cold with an empty response which must be fetched.
@@ -24,5 +34,5 @@ end
 if type(ledge.config.on_after_send) == 'function' then
     response = ledge.config.on_after_send(ledge, response)
 else
-    ngx.log(ngx.NOTICE, "on_after_send event handler is not a function")
+    --ngx.log(ngx.NOTICE, "on_after_send event handler is not a function")
 end
