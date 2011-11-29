66d1bf9af84eb91b061abcc6958b561a24a69a3f
= <jhurst@squiz.co.uk>
2011-11-29 17:35:09 +0000

    Think this is just tabs/spaces

--- lib/redis.lua
+++ lib/redis.lua
@@ -9,17 +9,17 @@ local redis = {
 -- @param	table	query expressed as a list of Redis commands
 -- @return	mixed	Redis response or false on failure
 function redis.query(query)
-	local rep = ngx.location.capture(ngx.var.redis_location, {
-		method = ngx.HTTP_POST,
-		args = { n = 1 },
-		body = redis.parser.build_query(query)
-	})
-	
-	if (rep.status == ngx.HTTP_OK) then
-		return redis.parser.parse_reply(rep.body)
-	else
-		return false
-	end
+    local rep = ngx.location.capture(ngx.var.redis_location, {
+        method = ngx.HTTP_POST,
+        args = { n = 1 },
+        body = redis.parser.build_query(query)
+    })
+
+    if (rep.status == ngx.HTTP_OK) then
+        return redis.parser.parse_reply(rep.body)
+    else
+        return false
+    end
 end
 
 
@@ -32,28 +32,28 @@ end
 -- @return	mixed	A table of parsed replies, or false on failure
 -- 
 function redis.query_pipeline(queries)
-	for i,q in ipairs(queries) do
-		queries[i] = redis.parser.build_query(q)
-	end
-	
-	local rep = ngx.location.capture(ngx.var.redis_location, {
-		args = { n = #queries },
-		method = ngx.HTTP_POST,
-		body = table.concat(queries)
-	})
-	
-	local reps = {}
-	
-	if (rep.status == ngx.HTTP_OK) then
-		local results = redis.parser.parse_replies(rep.body, #queries)
-		for i,v in ipairs(results) do
-			table.insert(reps, v[1]) -- #1 = res, #2 = typ
-		end
-		
-		return reps
-	else
-		return false
-	end
+    for i,q in ipairs(queries) do
+        queries[i] = redis.parser.build_query(q)
+    end
+
+    local rep = ngx.location.capture(ngx.var.redis_location, {
+        args = { n = #queries },
+        method = ngx.HTTP_POST,
+        body = table.concat(queries)
+    })
+
+    local reps = {}
+
+    if (rep.status == ngx.HTTP_OK) then
+        local results = redis.parser.parse_replies(rep.body, #queries)
+        for i,v in ipairs(results) do
+            table.insert(reps, v[1]) -- #1 = res, #2 = typ
+        end
+
+        return reps
+    else
+        return false
+    end
 end
 
 return redis
