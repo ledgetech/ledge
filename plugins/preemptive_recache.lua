df5ca0e89897c416d6b89f836c8f1dc729dcdf3b
= <jhurst@squiz.co.uk>
2012-02-14 12:22:39 +0000

    Minor

--- plugins/preemptive_recache.lua
+++ plugins/preemptive_recache.lua
@@ -3,7 +3,11 @@ local preemptive_recache = {}
 function preemptive_recache.go(ledge, response)
     -- Add this response to the sorted set
     if response.ttl ~= nil then
-        ledge.redis.query({ 'ZADD', 'ledge:recache', ngx.time() + response.ttl, response.keys.uri })
+        ledge.redis.query({ 'ZADD', 
+                            'ledge:recache', 
+                            ngx.time() + response.ttl, 
+                            response.keys.uri 
+        })
         
         -- Anything about to die?
         local expiring = ledge.redis.query({ 'ZRANGEBYSCORE', 'ledge:recache', 1, 60 })
