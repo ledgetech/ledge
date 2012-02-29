0fa7be7795e1e8d0109a92628620c23fd09ad73b
= <jhurst@squiz.co.uk>
2012-02-29 14:22:53 +0000

    Added an ESI config example

--- config.lua
+++ config.lua
@@ -7,7 +7,7 @@ local event = require("lib.event")
 -- request. The other requests wait until the first one finishes. 
 -- Suits slow but cacheable origins. Probably turn this off for 
 -- URIs which are deifinitely not cacheable.
-config.set("collapse_origin_requests", true)
+config.set("collapse_origin_requests", false)
 
 -- serve_when_stale
 --
@@ -17,6 +17,7 @@ config.set("serve_when_stale", 3600, {
     match_uri = { 
         { "^/about", 250 },
         { "^/contact", 350 },
+        { "^/esi%-fragments/time", 60 },
     } 
 })
 
@@ -32,10 +33,42 @@ config.set("serve_when_stale", 3600, {
 --
 -- Example:
 --
---      events.listen("response_ready", function() 
+--      event.listen("response_ready", function() 
 --          ngx.log(ngx.NOTICE, "My content is ready to be sent")
 --      end)
 
-event.listen("response_sent", function() 
-    ngx.log(ngx.NOTICE, "My stale config setting was  " .. ngx.ctx.config.serve_when_stale)
+
+-- A basic ESI parser
+event.listen("response_ready", function()
+    local response = ngx.ctx.response
+
+    -- We can't do ngx.location.capture within a Lua callback, so we must fetch
+    -- in advance, and then swap afterwards.
+    local tags = {response.body:match('(<esi:include.-/>)')}
+    local uris = {}
+
+    for i,tag in ipairs(tags) do
+        local _, _, src = tag:find('src="(.-)"')
+
+        local fragment = ngx.location.capture(src, {
+            method = ngx.HTTP_GET,
+        })
+
+        if fragment.status == ngx.HTTP_OK then
+            uris[src] = fragment
+        else 
+            ngx.log(ngx.NOTICE, "No response: " ..fragment.status)
+        end
+    end
+    
+    -- Now actually do the replacement.
+    response.body = response.body:gsub('(<esi:include.-/>)', function(tag)
+        local _, _, src = tag:find('src="(.-)"')
+        return uris[src].body
+    end)
+    ngx.ctx.response = response
+end)
+
+event.listen("response_sent", function()
+    ngx.log(ngx.NOTICE, "My stale config was: " .. ngx.ctx.config.serve_when_stale)
 end)
