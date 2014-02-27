f6d14c5fa6216f8102bb6c8a016e41f5025ba294
James Hurst <james@pintsized.co.uk>
2014-02-27 09:37:53 +0000

    Updated module syntax

--- lib/ledge/header_util.lua
+++ lib/ledge/header_util.lua
@@ -1,27 +1,32 @@
 local tonumber = tonumber
 local setmetatable = setmetatable
-local ngx = ngx
-
-module(...)
-
-_VERSION = "0.01"
-
-local mt = { __index = _M }
+local ngx_re_match = ngx.re.match
+local str_find = string.find
+local str_gsub = string.gsub
 
 
-function header_has_directive(header, directive)
+local _M = {
+    _VERSION = 0.02
+}
+
+local mt = {
+    __index = _M,
+}
+
+
+function _M.header_has_directive(header, directive)
     if header then
         -- Just checking the directive appears in the header, e.g. no-cache, private etc.
-        return (header:find(directive, 1, true) ~= nil)
+        return (str_find(header, directive, 1, true) ~= nil)
     end
     return false
 end
 
 
-function get_header_token(header, directive)
-    if header_has_directive(header, directive) then
+function _M.get_header_token(header, directive)
+    if _M.header_has_directive(header, directive) then
         -- Want the string value from a token
-        local value = ngx.re.match(header, directive:gsub('-','\\-').."=\"?([a-z0-9_~!#%&'`\\$\\*\\+\\-\\|\\^\\.]+)\"?", "ioj")
+        local value = ngx_re_match(header, str_gsub(directive, '-','\\-').."=\"?([a-z0-9_~!#%&'`\\$\\*\\+\\-\\|\\^\\.]+)\"?", "ioj")
         if value ~= nil then
             return value[1]
         end
@@ -31,10 +36,10 @@ function get_header_token(header, directive)
 end
 
 
-function get_numeric_header_token(header, directive)
-    if header_has_directive(header, directive) then
+function _M.get_numeric_header_token(header, directive)
+    if _M.header_has_directive(header, directive) then
         -- Want the numeric value from a token
-        local value = ngx.re.match(header, directive:gsub('-','\\-').."=\"?(\\d+)\"?", "ioj")
+        local value = ngx_re_match(header, str_gsub(directive, '-','\\-').."=\"?(\\d+)\"?", "ioj")
         if value ~= nil then
             return tonumber(value[1])
         end
@@ -43,13 +48,4 @@ function get_numeric_header_token(header, directive)
     return 0
 end
 
-
-local class_mt = {
-    -- to prevent use of casual module global variables
-    __newindex = function (table, key, val)
-        error('attempt to write to undeclared variable "' .. key .. '"')
-    end
-}
-
-
-setmetatable(_M, class_mt)
+return setmetatable(_M, mt)
