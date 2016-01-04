2c40f03dd2af6be5ad49a223995975b5b77fa256
James Hurst <james@pintsized.co.uk>
2016-01-04 15:20:45 +0000

    Version bump

--- lib/ledge/ledge.lua
+++ lib/ledge/ledge.lua
@@ -165,7 +165,7 @@ end
 
 
 local _M = {
-    _VERSION = '1.17',
+    _VERSION = '1.18',
 
     ORIGIN_MODE_BYPASS = 1, -- Never go to the origin, serve from cache or 503.
     ORIGIN_MODE_AVOID  = 2, -- Avoid the origin, serve from cache where possible.
