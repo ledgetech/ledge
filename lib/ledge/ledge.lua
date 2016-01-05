b1b5531fb0015f0e259f5608328d97759a99e265
James Hurst <james@pintsized.co.uk>
2016-01-05 13:18:03 +0000

    Fixed error handling (properly)

--- lib/ledge/ledge.lua
+++ lib/ledge/ledge.lua
@@ -2559,7 +2559,7 @@ function _M.delete_from_cache(self)
         if not size or size == ngx_null then
             size = 60
             ngx_log(ngx_ERR,    "could not determine entity size for scheduling GC, "
-                                .. "will collect in 60 seconds: " .. err or "")
+                                .. "will collect in 60 seconds: " .. (err or ""))
         end
 
         self:put_background_job("ledge", "ledge.jobs.collect_entity", {
