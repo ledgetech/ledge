3a09b7b1f37b61231ac09d0924e3d6f621638615
James Hurst <james@pintsized.co.uk>
2016-05-16 11:46:58 +0100

    Revalidate on purge for wildcard purge jobs - not pretty right now

--- lib/ledge/jobs/purge.lua
+++ lib/ledge/jobs/purge.lua
@@ -1,10 +1,12 @@
 local redis_connector = require "resty.redis.connector"
 local ledge = require "ledge.ledge"
+local qless = require "resty.qless"
 
 local ipairs, tonumber = ipairs, tonumber
 local str_len = string.len
 local str_sub = string.sub
 local ngx_null = ngx.null
+local ngx_md5 = ngx.md5
 
 local _M = {
     _VERSION = '0.01',
@@ -17,6 +19,7 @@ function _M.perform(job)
     local redis = job.redis
     local redis_params = job.redis_params
     local redis_connection_options = job.redis_connection_options
+    local redis_qless_database = job.redis_qless_database
 
     -- Try to connect to a slave for SCAN commands.
     local rc = redis_connector.new()
@@ -39,10 +42,14 @@ function _M.perform(job)
     local res, err = _M.expire_pattern(
         redis,
         redis_slave,
+        redis_params,
+        redis_qless_database,
         0,
         job.data.key_chain,
         job.data.keyspace_scan_count,
-        job
+        job,
+        job.data.revalidate,
+        job.data.delete
     )
 
     if res ~= nil then
@@ -62,7 +69,11 @@ end
 --  cursor: the scan cursor, updated for each iteration
 --  key_chain: key chain containing the patterned key to scan for
 --  count: the scan count size
-function _M.expire_pattern(redis, redis_slave, cursor, key_chain, count, job)
+--  job: the qless job
+--  revalidate: whether to schedule a background revalidate
+--  delete: whether to hard delete rather than expire
+function _M.expire_pattern(redis, redis_slave, redis_params, redis_qless_database, cursor,
+                            key_chain, count, job, revalidate, delete)
     local res, err = redis_slave:scan(
         cursor,
         "MATCH", key_chain.key,
@@ -80,13 +91,34 @@ function _M.expire_pattern(redis, redis_slave, cursor, key_chain, count, job)
     else
         for _,key in ipairs(res[2]) do
             local entity = redis:get(key)
-            if entity then
+            if entity and entity ~= ngx_null then
                 -- Remove the ::key part to give the cache_key without a suffix
                 local cache_key = str_sub(key, 1, -(str_len("::key") + 1))
+                -- the entity keys for the live entity
+                local entity_keys = ledge.entity_keys(nil, cache_key .. "::" .. entity)
+
+                if revalidate then
+                    local uri, err = redis:hget(entity_keys.main, "uri")
+                    if not uri or uri == ngx_null then
+                        return nil, err
+                    end
+
+                    -- Schedule the background job (immediately). jid is a function of the
+                    -- URI for automatic de-duping.
+                    _M.put_background_job(redis_params, redis_qless_database, "ledge", "ledge.jobs.revalidate", {
+                        uri = uri,
+                        entity_keys = entity_keys,
+                    }, {
+                        jid = ngx_md5("revalidate:" .. uri),
+                        tags = { "revalidate" },
+                        priority = 5,
+                    })
+                end
+
                 local res = ledge.expire_keys(
                     redis,
                     ledge.key_chain(nil, cache_key), -- a keychain for this key
-                    ledge.entity_keys(nil, cache_key .. "::" .. entity) -- the entity keys for the live entity
+                    entity_keys
                 )
             end
         end
@@ -94,11 +126,59 @@ function _M.expire_pattern(redis, redis_slave, cursor, key_chain, count, job)
         local cursor = tonumber(res[1])
         if cursor > 0 then
             -- If we have a valid cursor, recurse to move on.
-            return _M.expire_pattern(redis, redis_slave, cursor, key_chain, count, job)
+            return _M.expire_pattern(
+                redis,
+                redis_slave,
+                redis_params,
+                redis_qless_database,
+                cursor,
+                key_chain,
+                count,
+                job,
+                revalidate,
+                delete
+            )
         end
 
         return true
     end
+end
+
+
+function _M.put_background_job(redis_params, qless_db, queue, klass, data, options)
+    -- Try to connect to a slave for SCAN commands.
+    local rc = redis_connector.new()
+    redis_params.db = qless_db
+    redis_params.role = "master"
+
+    local redis, err = rc:connect(redis_params)
+
+    if not redis then
+        return nil, "job-error", "no redis connection provided"
+    end
+
+    redis:select(qless_db)
+    local q = qless.new({ redis_client = redis })
+    -- If we've been specified a jid (i.e. a non random jid), putting this
+    -- job will overwrite any existing job with the same jid.
+    -- We test for a "running" state, and if so we silently drop this job.
+    if options.jid then
+        local existing = q.jobs:get(options.jid)
+
+        if existing and existing.state == "running" then
+            return nil, "Job with the same jid is currently running"
+        end
+    end
+
+    -- Put the job
+    local res, err = q.queues[queue]:put(klass, data, options)
+    if not res then
+        ngx.log(ngx.ERR, err)
+    end
+
+    redis:set_keepalive()
+
+    return res, err
 end
 
 
